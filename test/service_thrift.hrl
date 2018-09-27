f51b91e4acd3818c15ba72105a455680d1effe68
yangyajun03 <yangyajun03@meituan.com>
2018-09-27 19:43:24 +0800

    code

---
+++ test/service_thrift.hrl
@@ -0,0 +1,5 @@
+-ifndef(_service_included).
+-define(_service_included, yeah).
+-include("javaBeansTest_types.hrl").
+
+-endif.
---
+++ test/srv_thrift.hrl
@@ -0,0 +1,5 @@
+-ifndef(_srv_included).
+-define(_srv_included, yeah).
+-include("debugProtoTest_types.hrl").
+
+-endif.
